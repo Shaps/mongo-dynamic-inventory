efb3e771257f579a5d1a4bcae31f399fb22ff667
Andrea Tartaglia <andrea.tartaglia@worldfirst.com>
2016-02-24 12:39:01 +0000

    refactor: Refactored for pep8 compliance

--- inventory.py
+++ inventory.py
@@ -2,6 +2,7 @@
 
 import argparse
 from pymongo import MongoClient
+
 try:
     import json
 except ImportError:
@@ -12,29 +13,30 @@ MONGO_PORT = "27017"
 
 
 def get_group_hosts(**kwargs):
-  group = kwargs['group']
-  if "ref_hosts" in group:
-    hosts = db.ansible_hosts.find({"_id": {"$in": group['ref_hosts']}})
-  else:
-    hosts = db.ansible_hosts.find({"vars.group_names": group['name']})
-  ret_list = []
-  for host in hosts:
-    ret_list.append(host['hostname'])
-  return sorted(ret_list)
+    group = kwargs['group']
+    if "ref_hosts" in group:
+        hosts = db.ansible_hosts.find({"_id": {"$in": group['ref_hosts']}})
+    else:
+        hosts = db.ansible_hosts.find({"vars.group_names": group['name']})
+    ret_list = []
+    for host in hosts:
+        ret_list.append(host['hostname'])
+    return sorted(ret_list)
 
 
 def get_group_vars(group):
-  ret_vars = {}
-  group_vars = db.groups.find_one({"name": group}, fields={"vars": True, "_id": False})
-  if group_vars is not None:
-    ret_vars = group_vars['vars']
+    ret_vars = {}
+    group_vars = db.groups.find_one({"name": group}, {"vars": True, "_id": False})
+    if group_vars is not None:
+        ret_vars = group_vars['vars']
 
-  return ret_vars
+    return ret_vars
 
 
 def get_host_vars(host):
-  host = db.ansible_hosts.find_one({"hostname": host})
-  return host['vars']
+    host = db.ansible_hosts.find_one({"hostname": host})
+    return host['vars']
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--list', action="store_true")
@@ -54,18 +56,19 @@ if args.list:
     all_group_vars = get_group_vars(all_group).items()
     groups = db.groups.find()
     for group in groups:
-      if group['name'] not in scanned_groups:
-        group_hosts = get_group_hosts(group=group)
-        json_docs.update({
-            group['name']: {
-                "hosts": group_hosts,
-                "vars": group['vars']
-            }
-        })
-        for host in group_hosts:
-          host_vars = get_host_vars(host)
-          json_docs['_meta']['hostvars'][host] = dict(list(host_vars.items() + group['vars'].items() + all_group_vars))
-        scanned_groups.append(group['name'])
+        if group['name'] not in scanned_groups:
+            group_hosts = get_group_hosts(group=group)
+            json_docs.update({
+                group['name']: {
+                    "hosts": group_hosts,
+                    "vars": group['vars']
+                }
+            })
+            for host in group_hosts:
+                host_vars = get_host_vars(host)
+                json_docs['_meta']['hostvars'][host] = dict(
+                    list(host_vars.items() + group['vars'].items() + all_group_vars))
+            scanned_groups.append(group['name'])
     if args.H:
         print json.dumps(json_docs, sort_keys=False, indent=4, separators=(',', ': '))
     else:
@@ -74,13 +77,13 @@ if args.list:
 
 elif args.host:
     host = args.host[0]
-    host = db.ansible_hosts.find_one({'hostname': host}, fields={"vars": True, "_id": False})
+    host = db.ansible_hosts.find_one({'hostname': host}, {"vars": True, "_id": False})
     if host:
-      for group in host["vars"]["group_names"]:
-        host["vars"] = dict(list(host['vars'].items() + get_group_vars(group).items()))
+        for group in host["vars"]["group_names"]:
+            host["vars"] = dict(list(host['vars'].items() + get_group_vars(group).items()))
     else:
-      host = {}
-      host['vars'] = {}
+        host = {}
+        host['vars'] = {}
 
     if args.H:
         print json.dumps(host["vars"], sort_keys=True, indent=4, separators=(',', ': '))
